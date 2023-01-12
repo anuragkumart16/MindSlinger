b24d67c30c7f1ae5cd273d207a27cb99c0c13551
Anurag <anuragkumartiwari12@gmail.com>
2023-01-12 23:44:21 +0530

    twicked app wrote heroku stufs and imports

--- mindslinger/settings.py
+++ mindslinger/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'mindslinger.urls'
@@ -118,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT= os.path.join(BASE_DIR,'static')
+STATICFILES_STORAGE= "whitenoise.storage.CompressManifestStaticFilesStorage"
 
 STATICFILES_DIRS=['static']
 
@@ -126,3 +130,5 @@ STATICFILES_DIRS=['static']
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
